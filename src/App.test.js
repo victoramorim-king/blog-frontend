e888f839a3e120053bffaa70ca1e631c8db4c37c
victor <victoramorimchagas@gmail.com>
2024-03-09 16:50:59 -0300

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Welcome message in the screen', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Welcome to my blog/);
   expect(linkElement).toBeInTheDocument();
 });
